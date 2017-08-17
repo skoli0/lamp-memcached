eabc67c07433ebd99ef65409d3ded8251df0292c
Sandeep Koli <Sandeep_Koli@symantec.com>
2017-08-17 11:29:31 +0100

    Open an app link using HTTP in default web browser

--- open_app_http.sh
+++ open_app_http.sh
@@ -7,6 +7,7 @@ set -x
 echo "Curl to app link in case server is installed without GUI..."
 curl -L -s http://localhost:8080/app
 
+# Open an app link using HTTP in default web browser
 echo "Open the same app link with default web browser in case server is installed with GUI..."
 export DISPLAY=:0.0
 sh -c "xdg-open http://localhost:8080/app &"
