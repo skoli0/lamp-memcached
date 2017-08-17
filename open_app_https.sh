9b3c243735bcbb99f7092b380c86872301b445c6
skoli0 <kolisandeep25@gmail.com>
2017-08-17 11:32:54 +0100

    Open an app link using HTTPS in default web browser

--- open_app_https.sh
+++ open_app_https.sh
@@ -3,11 +3,12 @@ set -x
 
 # -------------------------------------------------------------------------------------------------------------------------------
 # [Author] skoli0
-#          Open an app link using HTTP in default web browser
+#          Open an app link using HTTPS in default web browser
 # -------------------------------------------------------------------------------------------------------------------------------
 echo "Curl to app link in case server is installed without GUI..."
 curl -L -s "https://localhost:8443/app"
 
+# Open an app link using HTTP in default web browser
 echo "Open the same app link with default web browser in case server is installed with GUI..."
 export DISPLAY=:0.0
 sh -c "xdg-open https://localhost:8443/app &"
