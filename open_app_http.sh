6d7eb9d0a08bbec6276c1be5ce7c3be4947876f7
Sandeep Koli <sandeep.akoli@gmail.com>
2017-09-04 08:55:36 +0530

    added test modification

--- open_app_http.sh
+++ open_app_http.sh
@@ -9,5 +9,6 @@ curl -L -s http://localhost:8080/app
 
 # Open an app link using HTTP in default web browser
 echo "Open the same app link with default web browser in case server is installed with GUI..."
+# Display environment variable
 export DISPLAY=:0.0
 sh -c "xdg-open http://localhost:8080/app &"
